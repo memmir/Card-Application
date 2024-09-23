0ec2000af66bb8064705c0a60f3d7426f6a4cac8
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-23 14:22:44 +0300

    Added; when click card, opened create pop-up

--- src/app/cards/card-item/card-item.component.ts
+++ src/app/cards/card-item/card-item.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Card} from '../../models/card';
+import {MatDialog} from '@angular/material/dialog';
+import {CardModalComponent} from '../card-modal/card-modal.component';
 
 @Component({
   selector: 'app-card-item',
@@ -10,9 +12,17 @@ export class CardItemComponent implements OnInit {
 
   @Input() card!: Card;
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  openUpdateCardModel(): void{
+    this.dialog.open(CardModalComponent, {
+      width: '400px',
+      data: this.card,
+    });
+  }
 }
